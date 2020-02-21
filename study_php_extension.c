15cb9bf867826d52398858ce831239f1208f87cf
youkidearitai <youkidearitai@gmail.com>
2020-02-22 02:48:30 +0900

    Add string, resource for dump function

--- study_php_extension.c
+++ study_php_extension.c
@@ -15,18 +15,11 @@
 	ZEND_PARSE_PARAMETERS_END()
 #endif
 
-/* {{{ void study_php_extension_dump( [ mixed $var ] )
- */
-PHP_FUNCTION(study_php_extension_dump)
+PHPAPI void study_php_extension_var_dump(zval *struc, int level) /* {{{ */
 {
-	zval *zv_ptr;
-	int argc;
+	char tmp_str[PHP_DOUBLE_MAX_LENGTH];
 
-	ZEND_PARSE_PARAMETERS_START(1, 1)
-		Z_PARAM_VARIADIC('+', zv_ptr, argc)
-	ZEND_PARSE_PARAMETERS_END();
-
-	switch(Z_TYPE_P(zv_ptr))
+	switch(Z_TYPE_P(struc))
 	{
 		case IS_NULL:
 			php_printf("NULL: null\n");
@@ -38,11 +31,52 @@ PHP_FUNCTION(study_php_extension_dump)
 			php_printf("BOOL: false\n");
 			break;
 		case IS_LONG:
-			php_printf("LONG: %ld\n", Z_LVAL_P(zv_ptr));
+			php_printf("LONG: %ld\n", Z_LVAL_P(struc));
+			break;
+		case IS_DOUBLE:
+			php_gcvt(Z_DVAL_P(struc), 17, '.', 'E', tmp_str);
+			php_printf("DOUBLE: %s", tmp_str);
+			if (zend_finite(Z_DVAL_P(struc)) && strchr(tmp_str, '.') == NULL) {
+				PHPWRITE(".0", 2);
+			}
+			PHPWRITE("\n", 1);
+			break;
+		case IS_STRING:
+			php_printf("STRING: value=\"");
+			PHPWRITE(Z_STRVAL_P(struc), Z_STRLEN_P(struc));
+			php_printf("\", length=%ld\n", Z_STRLEN_P(struc));
+			break;
+		case IS_RESOURCE: {
+			const char *type_name = zend_rsrc_list_get_rsrc_type(Z_RES_P(struc));
+			php_printf("RESOURCE: id=%d type=%s\n", Z_RES_P(struc)->handle, type_name ? type_name : "Unknown");
+			break;
+		}
+		case IS_ARRAY:
+			php_printf("ARRAY: hashtable=%p\n", Z_ARRVAL_P(struc));
+			break;
+		case IS_OBJECT:
+			php_printf("OBJECT: ???\n");
 			break;
 		default:
 			php_printf("UNKNOWN\n");
 			break;
+	}
+}
+/* }}} */
+
+/* {{{ void study_php_extension_dump( [ mixed $var ] )
+ */
+PHP_FUNCTION(study_php_extension_dump)
+{
+	zval *zv_ptr;
+	int argc, i;
+
+	ZEND_PARSE_PARAMETERS_START(1, 1)
+		Z_PARAM_VARIADIC('+', zv_ptr, argc)
+	ZEND_PARSE_PARAMETERS_END();
+
+	for (i = 0; i < argc; i++) {
+		study_php_extension_var_dump(&zv_ptr[i], 1);
 	}
 }
 
--- tests/002.phpt
+++ tests/002.phpt
@@ -13,6 +13,12 @@ study_php_extension_dump(true);
 study_php_extension_dump(false);
 study_php_extension_dump(1);
 study_php_extension_dump(-30);
+study_php_extension_dump(3.1415);
+study_php_extension_dump(2.0);
+study_php_extension_dump(-2.0);
+study_php_extension_dump("Hello World");
+study_php_extension_dump("");
+study_php_extension_dump(fopen("/dev/null", "r"));
 ?>
 --EXPECT--
 NULL: null
@@ -20,3 +26,9 @@ BOOL: true
 BOOL: false
 LONG: 1
 LONG: -30
+DOUBLE: 3.1415000000000002
+DOUBLE: 2.0
+DOUBLE: -2.0
+STRING: value="Hello World", length=11
+STRING: value="", length=0
+RESOURCE: id=5 type=file
